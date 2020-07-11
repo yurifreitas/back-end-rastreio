8f84c784a46dc24801dd36dd9d2923df0ed6a18e
Yuri <yuribzfreitas@gmail.com>
2020-07-11 15:37:20 -0300

    second

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: "postgres",
-        host: "${process.env.DATABASE_HOST}",
-        port: "${process.env.DATABASE_PORT}",
-        database: "${process.env.DATABASE_NAME}",
-        username: "${process.env.DATABASE_USERNAME}",
-        password: "${process.env.DATABASE_PASSWORD}",
+        host: env('DATABASE_HOST'),
+        port: env('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
         ssl: { "rejectUnauthorized": false }
       },
       options: {
