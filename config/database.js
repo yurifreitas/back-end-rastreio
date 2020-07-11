4a1342e93343391d9f109a37b1993ceabca3c30d
Yuri <yuribzfreitas@gmail.com>
2020-07-11 15:43:46 -0300

    corec

--- config/database.js
+++ config/database.js
@@ -4,16 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: "postgres",
-        host: env('DATABASE_HOST'),
-        port: env('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: { "rejectUnauthorized": false }
-      },
-      options: {
-        useNullAsDefault: true,
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: 'public',
       },
     },
   },
